8c2f61f0878613b2d7ff160dfbcc3b35f1872cc2
satrioongrh <ngrhsatrio9@gmail.com>
2023-05-28 07:28:00 +0700

    menambahkan fragment, bottomnav click, cameraactivity

--- app/src/main/java/com/example/frutify/ui/dashboard/edit/EditActivity.kt
+++ app/src/main/java/com/example/frutify/ui/dashboard/edit/EditActivity.kt
@@ -1,12 +1,20 @@
 package com.example.frutify.ui.dashboard.edit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.frutify.R
+import com.example.frutify.databinding.ActivityEditBinding
+import com.example.frutify.ui.dashboard.camera.CameraActivity
 
 class EditActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.storyImage.setOnClickListener { startActivity(Intent(this, CameraActivity::class.java)) }
     }
 }
--- app/src/main/java/com/example/frutify/ui/dashboard/home/HomeFragment.kt
+++ app/src/main/java/com/example/frutify/ui/dashboard/home/HomeFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.frutify.R
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +16,10 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
